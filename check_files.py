ccee62d3423a70b1b3e21f4095602354a7040c9b
Coleman <gibsonjc@rose-hulman.edu>
2016-12-10 20:45:51 -0500

    added file to check for visitors

---
+++ check_files.py
@@ -0,0 +1,27 @@
+
+import os
+
+DEAD_TO_US = ['ClassVisitor',
+              'MethodVisitor',
+              'FieldVisitor',
+              'Visitor',
+              'visit']
+
+
+def check_files_for_badness(rootdir):
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            path = os.path.join(subdir, file)
+            f = open(path, 'r')
+            s = f.read()
+            we_did_bad = map(lambda name: name in s, DEAD_TO_US)
+            if True in we_did_bad:
+                return True
+    return False
+
+if __name__ == '__main__':
+
+    if check_files_for_badness('./src'):
+        print('We did bad')
+    else:
+        print('We did good')
