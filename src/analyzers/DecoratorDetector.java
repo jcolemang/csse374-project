b96c4889731976a1e6d11946d0806e5766a01a0b
Coleman <gibsonjc@rose-hulman.edu>
2017-02-09 12:10:26 -0500

    Finished (I think) the decorator detector
    We still need to look up from the abstract decorator

--- src/analyzers/DecoratorDetector.java
+++ src/analyzers/DecoratorDetector.java
@@ -1,7 +1,11 @@
 package analyzers;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import graphNodes.AbstractClassVertex;
 import graphNodes.IClassEdge;
 import graphNodes.IClassVertex;
@@ -16,49 +20,106 @@ public class DecoratorDetector extends AbstractAnalyzer {
 	//Also go up one more from the core abstract class
 	//to the interface
 
+	private Map<String, Boolean> abstractDecoratorMap = new HashMap<>();
+	private Map<String, Boolean> extendsAbstract = new HashMap<>();
+
 	@Override
 	public void analyze(IClassVertex v, ClassNodeGraph g, DOMGraph d) {
 
-		this.setVisited(v);
-		
-		if (v instanceof RegularClassVertex) {
-			IClassVertex abs;
-			
-			if (v.getSuperclassEdge() != null) {
-				abs = v.getSuperclassEdge().getTo();				
-			} else {
-				return;
-			}
-			if (abs != null
-					&& extendsAbstract(v)) { //if extends something and that thing is abstract
-				for (FieldData f : v.getFields()) {
-					if (f.getFieldType().getTitle().equals(abs.getTitle())) { //ewwwww
-						makeGreen(v);
-						makeGreen(abs);
-					}
-				}
-			}
-			
+		if (!(v instanceof RegularClassVertex)) {
+			return;
+		}
+
+        IClassVertex abs;
+
+        if (v.getSuperclassEdge() == null) {
+            return;
+        }
+
+        abs = v.getSuperclassEdge().getTo();
+
+        // if extends something and that thing is abstract
+        if (isAbstractDecorator(v) ||
+				extendsAbstractDecorator(v)) {
+			makeGreen(v);
+			makeGreen(abs);
+
 			//Turns interface(s) green
 			List<IClassEdge> intForAbs = abs.getImplementsEdges();
-			if(intForAbs != null) {
-				for (IClassEdge e : intForAbs) {
-					makeGreen(e.getTo());
-				}
-			}
-		}
-		
+            for (IClassEdge e : intForAbs) {
+                this.setVisited(e.getTo());
+                makeGreen(e.getTo());
+            }
+        }
+
 	}
-	
-	public boolean extendsAbstract(IClassVertex v) {
-		for (IClassEdge e : v.getEdges()) {
-			if (e.getTo() instanceof AbstractClassVertex) {
-				return true;
+
+
+	private boolean isAbstractDecorator(IClassVertex v) {
+
+		if (this.abstractDecoratorMap.getOrDefault(v.getTitle(), false)) {
+			return true;
+		}
+
+		if (!(v instanceof AbstractClassVertex)) {
+			this.abstractDecoratorMap.put(v.getTitle(), false);
+			return false;
+		}
+
+		// getting all possible superclasses
+        List<IClassVertex> potentialSupers = new LinkedList<>();
+		if (v.getSuperclassEdge() != null) {
+			potentialSupers.add(v.getSuperclassEdge().getTo());
+		}
+		for (IClassEdge edge : v.getImplementsEdges()) {
+			potentialSupers.add(edge.getTo());
+		}
+
+		// checking if, for any of the superclasses, the current class contains them as
+		// a field and overrides all of their methods
+        for (IClassVertex s : potentialSupers) {
+			if (v.containsField(s)) {
+				this.abstractDecoratorMap.put(v.getTitle(), true);
+			    return true;
 			}
 		}
+
+		this.abstractDecoratorMap.put(v.getTitle(), false);
 		return false;
 	}
-	
+
+
+	public boolean extendsAbstractDecorator(IClassVertex v) {
+
+		// checking if this has been visited
+		if (this.extendsAbstract.containsKey(v.getTitle())) {
+			if (this.extendsAbstract.get(v.getTitle()))	{
+				return true;
+			}
+
+			return false;
+		}
+
+		// getting the supertype
+		IClassEdge superTypeEdge = v.getSuperclassEdge();
+		if (superTypeEdge == null) {
+			return false;
+		}
+		IClassVertex superType = v.getSuperclassEdge().getTo();
+
+		// do I extend an abstract decorator, or does my supertype extend an
+		// abstract decorator?
+		if (this.isAbstractDecorator(superType) ||
+				this.extendsAbstractDecorator(superType)) {
+		    this.extendsAbstract.put(v.getTitle(), true);
+		    return true;
+		}
+
+		this.extendsAbstract.put(v.getTitle(), false);
+		return false;
+	}
+
+
 	public void makeGreen(IClassVertex v) {
 		if (v.getCorrespondingDOMNode() != null) {
 			v.getCorrespondingDOMNode().addAttribute("color", "\"green\"");			
